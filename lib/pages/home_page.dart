49d482b8a9a5a8a1bdb19720e9bf60359d9d8ad8
BogibekDev <bogibekdev@gmail.com>
2023-04-13 16:00:23 +0500

    finished

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("UI"),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              "User",
+              style: TextStyle(color: Colors.red, fontSize: 26),
+            ),
+            Text(
+              "Interface",
+              style: TextStyle(color: Colors.green, fontSize: 26),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
